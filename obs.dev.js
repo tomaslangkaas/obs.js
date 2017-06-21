ef94ae36b87d95180cb5bdce2535fa9a94d6a315
tomaslangkaas <tomaslangkaas@users.noreply.github.com>
2017-06-21 17:47:57 +0200

    Update obs.dev.js

--- obs.dev.js
+++ obs.dev.js
@@ -1,4 +1,4 @@
-var obs = function() {
+var obs = function(init) {
   var data = {},
     observers = {},
     iter = 1;
@@ -6,51 +6,57 @@ var obs = function() {
   function notify(prop, val, i) {
     val = getPropVal(data, prop);
     for(i in observers) {
-      observers[i] && observers[i](prop, val);
+      observers[i] && observers[i](prop, val, obs);
     }
   }
 
   function getPropVal(obj, prop, preserve) {
     return obj.hasOwnProperty(prop) ?
-      typeof obj[prop] === "function" && !preserve ? obj[prop]() :
-      obj[prop] :
+      typeof obj[prop] === "function" && !preserve ? 
+        obj[prop](obs) : obj[prop] :
       void 0;
   }
-  return function(prop, val) {
-    var preserve = prop === null,
+
+  function obs(arg1, arg2) {
+    var preserve = arg1 === null,
       args = arguments,
       i,
       target,
       source,
       copy = !args.length || preserve;
-    if(copy || typeof prop === "object") {
+    if(copy || typeof arg1 === "object") {
       if(!copy) {
-        for(i in prop) {
-          data[i] = prop[i];
+        for(i in arg1) {
+          data[i] = arg1[i];
         }
       }
       target = copy ? {} : data;
-      source = copy ? data : prop;
+      source = copy ? data : arg1;
       for(i in data) {
-        val = getPropVal(source, i, preserve);
-        if(copy ^ (val === void 0)) {
-          target[i] = val;
-        }!copy && notify(i);
+        arg2 = getPropVal(source, i, preserve);
+        if(copy ^ (arg2 === void 0)) {
+          target[i] = arg2;
+        }
+        !copy && notify(i);
       }
-      return copy ? target : this;
-    } else if(typeof prop === "function") {
+      return copy ? target : obs;
+    } else if(typeof arg1 === "function") {
       return(function(index) {
-        observers[index] = prop;
+        observers[index] = arg1;
         return function() {
           observers[index] = 0;
         };
       })(iter++);
     } else {
       if(1 in args) {
-        data[prop] = val;
-        notify(prop);
+        data[arg1] = arg2;
+        notify(arg1);
       }
-      return getPropVal(data, prop);
+      return getPropVal(data, arg1);
     }
   };
+  if(init) {
+    obs(init);
+  }
+  return obs;
 };
